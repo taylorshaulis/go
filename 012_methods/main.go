7b4dd074f6566c32849718f9544a9775c230bee1
Taylor Shaulis <lifes.short.play.hard@gmail.com>
2024-02-12 15:15:03 -0700

    Doing the characters as a map instead of unique variables

--- 012_methods/main.go
+++ 012_methods/main.go
@@ -23,4 +23,11 @@ func main() {
 	fmt.Printf("The class of %v is %v.\n", gimli.Name, gimli.Class)
 	gandalf.Race = "Istari"
 	gandalf.PrintWhoIs()
+
+
+	m := make(map[string]Character)
+	m["gandalf"] = Character{"Gandalf","Wizzard","Ainur"}
+	m["frodo"] = Character{"Frodo","Warlock","Half-Foot"}
+	fmt.Printf("The race of %v is %v.\n", m["gandalf"].Name, m["gandalf"].Race)
+	m["frodo"].PrintWhoIs()
 }
